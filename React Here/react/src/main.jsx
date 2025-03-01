fa90a2feeb2a157387df2d75f880060154204e60
aryal05 <aryal.rajat05@gmail.com>
2025-03-01 21:05:52 +0545

    creating own librabry and behind the work

--- React Here/react/src/main.jsx
+++ React Here/react/src/main.jsx
@@ -1,8 +1,15 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot,} from 'react-dom/client'
 import App from './App.jsx'
+
+// const reactElement = React.createElement(
+//     'a',
+//     {href: "https://google.com", target: "_blank"},
+//     "Click me here"
+// )
 
 createRoot(document.getElementById('root')).render(
 
     <App />
+    // reactElement
 )

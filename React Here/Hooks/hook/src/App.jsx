eef17885e1956ad14841a02ac4dfabd8105c3663
aryal05 <aryal.rajat05@gmail.com>
2025-03-03 20:12:51 +0545

    react hooks

---
+++ React Here/Hooks/hook/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React with Chai.</h1>
+      <h2>Counter Value: 5</h2>
+      <button>Add Value</button>
+      <button>Delete Value</button>
+    </>
+  )
+}
+
+export default App

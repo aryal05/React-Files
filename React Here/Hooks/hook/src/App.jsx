7ab39087436aebb930075bbb6c55f9b0aa73aaf7
aryal05 <aryal.rajat05@gmail.com>
2025-03-03 20:37:51 +0545

    usestate hook

--- React Here/Hooks/hook/src/App.jsx
+++ React Here/Hooks/hook/src/App.jsx
@@ -4,17 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-let counter = 5;
-const addValue = ()=>{
-  counter+=1;
-  console.log("Clicked", counter)
-}
+// let counter = 5;
+// const addValue = ()=>{
+//   counter+=1;
+//   setCounter(counter)
+//   console.log("Clicked", counter)
+// }
+let [counter ,setCounter] = useState(0)
   return (
     <>
-      <h1>React with Chai.</h1>
+     <h1>React with Chai.</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
-      <button>Delete Value</button>
+      <button onClick={() => setCounter(counter + 1)}>Add Value</button>
+      <button onClick={() => setCounter(Math.max(0, counter - 1))}>Delete Value</button>
     </>
   )
 }

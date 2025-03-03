c08cad21920b8cb9cb0219e284b7ba1dbc45c07b
aryal05 <aryal.rajat05@gmail.com>
2025-03-03 20:24:05 +0545

    console check

--- React Here/Hooks/hook/src/App.jsx
+++ React Here/Hooks/hook/src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+let counter = 5;
+const addValue = ()=>{
+  counter+=1;
+  console.log("Clicked", counter)
+}
   return (
     <>
       <h1>React with Chai.</h1>
-      <h2>Counter Value: 5</h2>
-      <button>Add Value</button>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
       <button>Delete Value</button>
     </>
   )

a1d956c75fbfcedcfdea1810e00918c0b352fe1e
aryal05 <aryal.rajat05@gmail.com>
2025-03-06 19:26:10 +0545

    irst commiut

--- React Here/Hooks/hook/src/App.jsx
+++ React Here/Hooks/hook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Card from './components/Card'
+// import Card from './components/Card'
 
 function App() {
 // let counter = 5;
@@ -9,20 +9,20 @@ function App() {
 //   setCounter(counter)
 //   console.log("Clicked", counter)
 // }
-
+b  
 // Reconciliation: The Algorithm React uses to diff one tree with another tree to determine which part need to be changed. 
 //It is the algorith behind what popularly understood as virtual dom. 
 
 let [counter ,setCounter] = useState(0)
   return (
     <>
-     {/* <h1>React with Chai.</h1>
+     <h1>React with Chai.</h1>
       <h2>Counter Value: {counter}</h2> 
       <div className='p-2'>
       <button className='bg-black mr-6 text-white rounded' onClick={() => setCounter(counter + 1)}>Add Value</button>
       <button className='bg-black text-white rounded' onClick={() => setCounter(Math.max(0, counter - 1))}>Delete Value</button>
-      </div> */}
-      <Card props = {"props here"} />
+      </div>
+      {/* <Card props = {"props here"} /> */}
      
     </>
   )

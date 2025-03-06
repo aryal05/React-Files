dea7ae1f847d669eb438013a11b22ae84dc9c48a
aryal05 <aryal.rajat05@gmail.com>
2025-03-06 20:07:29 +0545

    setting up the botton

--- React Here/Backgound Color Chnager/color-changer/src/App.jsx
+++ React Here/Backgound Color Chnager/color-changer/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("yellow")
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="w-full h-screen duration-200"
+     style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 px-2 inset-x-0">
+        <div className="flex flex-wrap gap-3 bg-white rounded-xl shadow-lg px-3 py-2">
+          <button className="outline-none px-4 py-2 shadow-lg bg-red-500 rounded-full text-white">Red</button>
+          <button className="outline-none px-4 py-2 shadow-lg bg-black rounded-full text-white">Black</button>
+          <button className="outline-none px-4 py-2 shadow-lg bg-red-500 rounded-full text-white">Pink</button>
+          <button className="outline-none px-4 py-2 shadow-lg bg-red-500 rounded-full text-white">Red</button>
+          <button className="outline-none px-4 py-2 shadow-lg bg-red-500 rounded-full text-white">Red</button>
+          <button className="outline-none px-4 py-2 shadow-lg bg-red-500 rounded-full text-white">Red</button>
+        </div>
+      </div> 
+
+
+     </div>
     </>
   )
 }

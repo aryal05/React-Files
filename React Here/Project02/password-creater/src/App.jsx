dacb7d2f958bde01d5eb860d3ef95d063f52fcb1
aryal05 <aryal.rajat05@gmail.com>
2025-03-08 20:19:34 +0545

    button

--- React Here/Project02/password-creater/src/App.jsx
+++ React Here/Project02/password-creater/src/App.jsx
@@ -23,11 +23,23 @@ function App() {
   return (
     <>
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
+      <h className='text-3xl text-center my-4'>Password Generator</h>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        <div className='flex text-m gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" name="" id=""
+            min={6}
+            max={100}
+            value={length} />
+          </div>
+        </div>
+
 
       </div>
     </div>
+    
     </>
   )
 }

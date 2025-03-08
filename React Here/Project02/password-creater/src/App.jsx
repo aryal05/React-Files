20969ba7d4a90bdf28fed870f1de6097b6216b7f
aryal05 <aryal.rajat05@gmail.com>
2025-03-08 15:22:34 +0545

    input field

--- React Here/Project02/password-creater/src/App.jsx
+++ React Here/Project02/password-creater/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useCallback } from 'react'
 
 
 function App() {
@@ -7,13 +7,27 @@ function App() {
   const [char, setChar] = useState (false);
   const [password, setPasssword] = useState("")
 
-  const passswordGenerator = ()=>{
+  const passswordGenerator = useCallback(()=>{
+    let pass = "";
+    let st = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(number) str+="0123456789"
+    if(char) str+="!@#$%^&*()_+"
+    for(let i=1;i<=length;i++){
+      pass += st.charAt(Math.floor(Math.random()*st.length))
 
-  }
+    }
+    setPasssword(pass)
+
+
+  },[length, number, char, setPasssword])
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generater</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly />
 
+      </div>
+    </div>
     </>
   )
 }

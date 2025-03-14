7738aa91bf1633a1dcfc98251ec9e85b15b636c9
aryal05 <aryal.rajat05@gmail.com>
2025-03-14 20:16:50 +0545

    optionss forr currrency

--- React Here/Project03/currency-converter/src/App.jsx
+++ React Here/Project03/currency-converter/src/App.jsx
@@ -1,12 +1,20 @@
+// import { useState } from 'react'
 import { useState } from 'react'
-
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-
+const[amount, setAmount] = useState(0)
+const [from, setFrom] = useState('USD')
+const [to, setTo] = useState('EUR')
+const[convertedAmount, setConvertedAmount]= useState(0)
+const currencyInfo = useCurrencyInfo(from)
+const options = Object.keys(currencyInfo)
 
   return (
     <>
       <h1 className='text-3xl text-center mx-2 my-4 bg-orange-500'>Currency App</h1>
+      <InputBox/>
     </>
   )
 }

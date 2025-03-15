19f04876da02a97777f215cd72f0515ecceef263
aryal05 <aryal.rajat05@gmail.com>
2025-03-15 09:53:43 +0545

    currency converter ui

--- React Here/Project03/currency-converter/src/App.jsx
+++ React Here/Project03/currency-converter/src/App.jsx
@@ -17,11 +17,51 @@ const swap =()=>{
   setConvertedAmount(amount)
   setAmount(convertedAmount)
 }
-
+const convert = ()=>{setConvertedAmount(amount*currencyInfo[to])}
   return (
     <>
-      <h1 className='text-3xl text-center mx-2 my-4 bg-orange-500'>Currency App</h1>
-      <InputBox/>
+     <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                // backgroundImage: `url('${BackgroundImage}')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                           
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert 
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
